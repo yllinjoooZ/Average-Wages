a8d476dc630428d753baeb13741c0ff9fb741737
yllinjoooZ <yllzeneli920@gmail.com>
2024-07-16 01:32:00 -0700

    Versioni 2
    Ekena bo pagen min edhe max

--- index.py
+++ index.py
@@ -1,8 +1,14 @@
 salary = [340, 210, 450, 600, 230, 500, 550, 300]
 shumaEpagave = 0
 for paga in salary:
-    shumaEpagave = shumaEpagave + paga
-    print(f"Shuma e pagave eshte: {shumaEpagave}")
+    shumaEpagave = shumaEpagave + paga 
+print(f"Shuma e pagave eshte: {shumaEpagave}")
     
-    pagaMesatare = shumaEpagave / len(salary)
-    print(f"Paga mesatare eshte: {pagaMesatare}")
+pagaMesatare = shumaEpagave / len(salary)
+print(pagaMesatare)
+
+pagaMax = salary[0]
+for pagaAktuale in salary:
+    if(pagaAktuale > pagaMax):
+        pagaMax = pagaAktuale
+        print(f"Paga maksimale eshte (pagaMax)")
